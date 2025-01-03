fbb21971a75cac7bf20835e814a3258167b418b7
Ronald Ossendrijver <ronald@ix-tech.nl>
2025-01-03 04:08:04 +0100

    Updated copyright notice

--- Server/Data/PersistedScheduledGame.cs
+++ Server/Data/PersistedScheduledGame.cs
@@ -1,5 +1,5 @@
 // /*
-//  * Copyright (C) 2020-2024 Ronald Ossendrijver (admin@treachery.online)
+//  * Copyright (C) 2020-2025 Ronald Ossendrijver (admin@treachery.online)
 //  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
 //  * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This
 //  * program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
