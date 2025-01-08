73f4e563cb7809d83a0ff319b4e3d8ee79b920cd
Ronald Ossendrijver <ronald@ix-tech.nl>
2025-01-08 02:50:13 +0100

    Db migration.

---
+++ Server/Migrations/20250107233339_RemovedRedundantBotsArePaused.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Treachery.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedRedundantBotsArePaused : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BotsArePaused",
+                table: "PersistedGames");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "BotsArePaused",
+                table: "PersistedGames",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
