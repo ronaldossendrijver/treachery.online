dd4a09ca1d2a4b916166d8fc37f54bf65b07ce94
Ronald Ossendrijver <ronaldossendrijver@hotmail.com>
2025-02-01 23:45:27 +0100

    Updated packages, created migration.

---
+++ Server/Migrations/20250201222850_NewFieldInPersistedGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Treachery.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewFieldInPersistedGame : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastAction",
+                table: "PersistedGames",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersistedGames_GameId",
+                table: "PersistedGames",
+                column: "GameId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PersistedGames_GameId",
+                table: "PersistedGames");
+
+            migrationBuilder.DropColumn(
+                name: "LastAction",
+                table: "PersistedGames");
+        }
+    }
+}
