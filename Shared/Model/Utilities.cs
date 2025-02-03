f73d5ce2f72d499aa129d5b17aa5c1d94169fe45
Ronald Ossendrijver <ronald@ix-tech.nl>
2025-02-03 11:18:43 +0100

    Checkpoint

--- Shared/Model/Utilities.cs
+++ Shared/Model/Utilities.cs
@@ -35,22 +35,38 @@ public static class Utilities
     private static readonly JsonSerializerOptions Options = new() { IncludeFields = true };
 
     private const string NewtonsoftIndicator = "Treachery.Shared";
-    private const string ToReplace1 = "\"$type\" : \"Treachery.Shared.GameState, Treachery.Shared\",";
-    private const string ToReplace2 = "\"$type\" : \"Treachery.Shared.GameEvent[], Treachery.Shared\",";
-    private const string Pattern = @"Treachery\.Shared\.([A-Za-z0-9_]+)|, Treachery\.Shared";
+    private const string Pattern = "\"\\$type\":\"Treachery\\.Shared\\.([A-Za-z0-9_\\[\\]]+), Treachery\\.Shared\",";
 
     public static T Deserialize<T>(string serialized)
     {
         if (serialized.Substring(0, 100).Contains(NewtonsoftIndicator))
         {
-            var cleaned = serialized.Replace(ToReplace1, string.Empty, StringComparison.InvariantCultureIgnoreCase).Replace(ToReplace2, string.Empty, StringComparison.InvariantCultureIgnoreCase);
-            string adjustedSerialized =  Regex.Replace(cleaned, Pattern, match => match.Groups[1].Success ? match.Groups[1].Value : "");
+            var adjustedSerialized = Regex.Replace(serialized, Pattern, match =>
+            {
+                string typeName = match.Groups[1].Value;
+                if (IsGameEvent(typeName))
+                {
+                    var newValue = "\"$type\":\"" + match.Value + "\",";
+                    
+                    return newValue;
+                }
+                else
+                {
+                    return string.Empty; // Remove the match
+                }
+            });
             return JsonSerializer.Deserialize<T>(adjustedSerialized, Options);
         }
         
         
         return JsonSerializer.Deserialize<T>(serialized, Options);
 
+    }
+    
+    private static bool IsGameEvent(string typeName)
+    {
+        var foundType = Type.GetType($"Treachery.Shared.{typeName}");
+        return foundType != null && !foundType.IsAbstract && !foundType.IsInterface && foundType.IsSubclassOf(typeof(GameEvent));
     }
 
     public static string Serialize<T>(T value)
